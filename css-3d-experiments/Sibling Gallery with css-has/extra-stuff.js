c87d74202515db0df2549552afcc720c2d6d4528
Eduard Alexandru <eduard.github@gmail.com>
2022-09-17 02:59:37 +0100

    3d linear gradient block, css sliding gallery with :has()

---
+++ css-3d-experiments/Sibling Gallery with css-has/extra-stuff.js
@@ -0,0 +1,51 @@
+const SUPPORT_MESSAGES = [
+    {
+      modifier: 'has',
+      message: 'This browser is not supported... yet. :has is currently supported in Chrome v105+, Chrome Canary with the "Experimental Web Platform Features" flag enabled or Safari.' 
+    },
+    {
+      modifier: 'popup',
+      message: 'This browser is not supported... yet. popup is currently supported in Chrome Canary with the "Experimental Web Platform Features" flag enabled.' 
+    },
+    {
+      modifier: 'houdini',
+      message: 'This browser is not supported... yet. Some Houdini features are currently supported in Chromium based browsers. Check <a href="https://ishoudinireadyyet.com/" rel="noopener noreferrer" target="_blank">ishoudinireadyyet.com</a>.' 
+    },
+    {
+      modifier: 'object-view-box',
+      message: 'This browser is not supported... yet. object-view-box is currently supported in Chrome Canary with the "Experimental Web Platform Features" flag enabled.' 
+    },
+    {
+      modifier: 'scroll-timeline',
+      message: 'This browser is not supported... yet. @scroll-timeline is currently supported in Chrome Canary with the "Experimental Web Platform Features" flag enabled.' 
+    },
+    {
+      modifier: 'set',
+      message: 'This browser is not supported... yet. The "Shared Element Transitions" API is currently supported in Chrome Canary with the "Experimental Web Platform Features" and "Document Transition" flags enabled.' 
+    },
+    {
+      modifier: 'anchoring',
+      message: 'This browser is not supported... yet. CSS anchoring can be experimented with in Chrome Canary with the "Experimental Web Platform Features" flag enabled.' 
+    }
+];
+    
+const generateWarnings = () => {
+    const CONTAINER = document.createElement('div');
+    CONTAINER.className = 'support-warnings';
+    for (const { modifier, message } of SUPPORT_MESSAGES) {
+        const WARNING = document.createElement('div');
+        WARNING.className = `support-warning support-warning--${modifier}`;
+        WARNING.innerHTML = message;
+        CONTAINER.appendChild(WARNING);
+    }
+    document.body.appendChild(CONTAINER);
+};
+
+generateWarnings();
+    
+// For JavaScript APIs
+if (Element.prototype.hasOwnProperty("popUp"))
+document.documentElement.classList.add('popup-supported');
+
+if ('createDocumentTransition' in document)
+document.documentElement.classList.add('set-supported');
